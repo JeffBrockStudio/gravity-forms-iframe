3459f546dcfb9265b671281e3272ed736a5be2aa
JeffBrockStudio <jeff@jeffbrockstudio.com>
2023-07-25 20:23:19 -0700

    Added webfonts to iframe.

--- templates/gravity-forms-iframe.php
+++ templates/gravity-forms-iframe.php
@@ -29,7 +29,13 @@ if ( $site != '' ):
 			} else {
 				$domain = 'https://northwestsewn.org';
 			}
-			$form_style = $domain . '/wp-content/themes/northwestsewn/css/gravity-forms.css';	
+			$form_style = $domain . '/wp-content/themes/northwestsewn/css/gravity-forms.css';	?>
+			<link rel="preconnect" href="https://fonts.googleapis.com">
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+			
+			<link rel="stylesheet" href="https://use.typekit.net/bkb6fpl.css">
+			<?php
 			break;
 
 	endswitch;
